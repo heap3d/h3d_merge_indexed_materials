5d7a99aab3aa6ff02b34c6b3c86509c69df4be7e
Dmytro Holub <heap3d@gmail.com>
2025-05-09 16:58:49 +0300

    initial commit

---
+++ scripts/merge_indexed_materials.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# ================================
+# (C)2025 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# merge indexed polygon tags, for example glass, glass (2), glass (3) etc.
+# ================================
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
